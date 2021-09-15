32e5e96fb2b1674a5679a584974f1fa7244e52fa
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-09-15 19:48:52 +0600

    init Project

---
+++ spring-boot-client-app/src/main/java/com/imranmadbar/SpringClientApplication.java
@@ -0,0 +1,12 @@
+package com.imranmadbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientApplication.class, args);
+    }
+}
---
+++ spring-boot-client-app/src/main/java/com/imranmadbar/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.imranmadbar.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String appHome() {
+        return "This is from home controller";
+    }
+
+
+}
