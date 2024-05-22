d2947da3a08d9a28c9b46c24175adde1bccb277b
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2024-05-22 15:54:14 +0600

    Update for springboot 3

--- spring-boot-maven-client-app/src/main/java/com/imranmadbar/HomeController.java
+++ spring-boot-maven-client-app/src/main/java/com/imranmadbar/HomeController.java
@@ -1,17 +1,37 @@
 package com.imranmadbar;
 
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 
-    @GetMapping("/home")
-    public ResponseEntity<String> hello(Authentication authentication) {
-        final String body = "Hello From Home: " + authentication.getName();
-        return ResponseEntity.ok(body);
-    }
+	@GetMapping("/home")
+	public ResponseEntity<String> hello(Authentication authentication) {
+		final String body = "Hello From Home: " + authentication.getName();
+		return ResponseEntity.ok(body);
+	}
+
+	@RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+	public ResponseEntity<String> getAnonymous() {
+		return ResponseEntity.ok("Hello Anonymous");
+	}
+
+	@PreAuthorize("hasRole('ROLE_user')")
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public ResponseEntity<String> getUser() {
+		return ResponseEntity.ok("Hello Secured with user role.");
+	}
+	
+	
+	
+	
+	
+	
 }
